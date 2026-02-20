97505c2e170e89d806ee11ae8a980be8cd93192a
Alex Vinyals <alex.vinyals@proton.me>
2026-02-20 22:11:25 +0100

    types/blueprint_book: implement `min_fwd_delta_chrono`

--- lib/src/types/experimental/blueprint_book.rs
+++ lib/src/types/experimental/blueprint_book.rs
@@ -1,3 +1,7 @@
+use chrono::DateTime;
+use chrono::Local;
+use chrono::TimeDelta;
+
 use crate::types::Blueprint;
 
 /// Models a collection of blueprints.
@@ -14,6 +18,14 @@ impl BlueprintBook {
     pub fn entries(&self) -> &[Blueprint] {
         &self.entries
     }
+
+    pub fn min_fwd_delta_chrono(&self, ts: DateTime<Local>) -> Option<TimeDelta> {
+        self.entries
+            .iter()
+            .map(|blueprint| blueprint.preferred_slot().fwd_delta_chrono(ts))
+            .filter(|delta| !delta.is_zero())
+            .min()
+    }
 }
 
 impl std::fmt::Display for BlueprintBook {
@@ -23,5 +35,40 @@ impl std::fmt::Display for BlueprintBook {
             f.write_str("\n")?;
         }
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use chrono::Local;
+    use chrono::TimeDelta;
+    use chrono::TimeZone;
+
+    use crate::types::Blueprint;
+    use crate::types::Duration;
+    use crate::types::HourSlot;
+    use crate::types::Priority;
+    use crate::types::Recurrence;
+    use crate::types::Slot;
+    use crate::types::TimeUnit;
+    use crate::types::experimental::blueprint_book::BlueprintBook;
+
+    #[test]
+    fn test_min_fwd_delta_chrono() {
+        let eight_am = Slot::Hour(HourSlot::Fixed { hour: 8 });
+        let morning = Slot::Hour(HourSlot::Range { start: 8, stop: 12 });
+        let daily = Recurrence::Period {
+            spacing: Duration::of(1, TimeUnit::Day),
+        };
+
+        let one_hour = Duration::of(1, TimeUnit::Hour);
+
+        let sut = BlueprintBook::from(vec![
+            Blueprint::new("1", "Task A", one_hour, Priority::Crit, daily, eight_am),
+            Blueprint::new("2", "Task B", one_hour, Priority::Norm, daily, morning),
+        ]);
+
+        let ts = Local.with_ymd_and_hms(2025, 10, 23, 14, 0, 0).unwrap();
+        assert_eq!(Some(TimeDelta::hours(18)), sut.min_fwd_delta_chrono(ts));
     }
 }
