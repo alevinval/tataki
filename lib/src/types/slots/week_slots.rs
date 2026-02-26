05b8527dc45a876f900b5dc6d5f2f3b6289e23dd
Alex Vinyals <alex.vinyals@proton.me>
2026-02-26 19:16:17 +0100

    types/week_slots: implement fwd_delta and chrono interop

--- lib/src/types/slots/week_slots.rs
+++ lib/src/types/slots/week_slots.rs
@@ -1,3 +1,8 @@
+use chrono::DateTime;
+use chrono::Datelike;
+use chrono::TimeDelta;
+use chrono::TimeZone;
+
 use crate::types::days::DayOfWeek;
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
@@ -46,6 +51,41 @@ impl WeekSlot {
             }
         }
     }
+
+    /// Computes the forward delta in days, if the input does not fit the slot,
+    /// rather than returning negative, it computes the delta until the next
+    /// week.
+    pub const fn fwd_delta(&self, src: DayOfWeek) -> i64 {
+        let src = src as u32;
+        let pivot = match self {
+            Self::Fixed { day } => {
+                let day = *day as u32;
+                if src <= day { day } else { day + 7 }
+            }
+            Self::Range {
+                from: start,
+                to: end,
+            } => {
+                let (start, end) = (*start as u32, *end as u32);
+                if src <= start {
+                    start
+                } else if src > end {
+                    start + 7
+                } else {
+                    src
+                }
+            }
+        };
+        pivot as i64 - src as i64
+    }
+
+    pub fn matches_chrono<T: TimeZone>(&self, src: DateTime<T>) -> bool {
+        self.matches(src.weekday().into())
+    }
+
+    pub fn fwd_delta_chrono<T: TimeZone>(&self, src: DateTime<T>) -> TimeDelta {
+        TimeDelta::days(self.fwd_delta(src.weekday().into()))
+    }
 }
 
 impl std::fmt::Display for WeekSlot {
@@ -59,13 +99,15 @@ impl std::fmt::Display for WeekSlot {
 
 #[cfg(test)]
 mod test {
-
     use super::*;
 
     mod slot_week {
         use super::*;
 
         mod fixed {
+
+            use chrono::Utc;
+
             use super::*;
 
             #[test]
@@ -76,6 +118,38 @@ mod test {
                 assert!(sut.matches(DayOfWeek::Wed));
                 assert!(!sut.matches(DayOfWeek::Tue));
                 assert!(!sut.matches(DayOfWeek::Thu));
+            }
+
+            #[test]
+            fn test_fwd_delta() {
+                let sut = WeekSlot::Fixed {
+                    day: DayOfWeek::Mon,
+                };
+                assert_eq!(4, sut.fwd_delta(DayOfWeek::Thu));
+
+                let sut = WeekSlot::Fixed {
+                    day: DayOfWeek::Tue,
+                };
+                assert_eq!(0, sut.fwd_delta(DayOfWeek::Tue));
+
+                let sut = WeekSlot::Fixed {
+                    day: DayOfWeek::Wed,
+                };
+                assert_eq!(5, sut.fwd_delta(DayOfWeek::Fri));
+            }
+
+            #[test]
+            fn test_chrono_interop() {
+                let sut = WeekSlot::Fixed {
+                    day: DayOfWeek::Tue,
+                };
+                let input = Utc.with_ymd_and_hms(2025, 10, 23, 14, 0, 0).unwrap();
+
+                assert_eq!(TimeDelta::days(5), sut.fwd_delta_chrono(input));
+
+                assert!(sut.matches_chrono(input - TimeDelta::days(2)));
+
+                assert!(!sut.matches_chrono(input));
             }
         }
 
@@ -95,6 +169,17 @@ mod test {
 
                 assert!(!sut.matches(DayOfWeek::Thu));
                 assert!(!sut.matches(DayOfWeek::Tue));
+            }
+
+            #[test]
+            fn test_fwd_delta() {
+                let sut = WeekSlot::Range {
+                    from: DayOfWeek::Wed,
+                    to: DayOfWeek::Fri,
+                };
+                assert_eq!(2, sut.fwd_delta(DayOfWeek::Mon));
+                assert_eq!(0, sut.fwd_delta(DayOfWeek::Thu));
+                assert_eq!(4, sut.fwd_delta(DayOfWeek::Sat));
             }
         }
     }
