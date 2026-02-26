a288268c5985b34cdc2c882d04897eb1bd6b2d35
Alex Vinyals <alex.vinyals@proton.me>
2026-02-26 19:16:17 +0100

    types/slots: implement matches_chrono and fwd_delta_chrono
    Delegate to the underlying implementation.

--- lib/src/types/slots.rs
+++ lib/src/types/slots.rs
@@ -1,4 +1,4 @@
-use chrono::Datelike;
+use chrono::TimeDelta;
 pub use hour_slots::HourSlot;
 pub use week_slots::WeekSlot;
 
@@ -7,7 +7,6 @@ mod week_slots;
 
 use chrono::DateTime;
 use chrono::TimeZone;
-use chrono::Timelike;
 
 /// A time slot for scheduling affinity.
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
@@ -19,11 +18,19 @@ pub enum Slot {
 }
 
 impl Slot {
-    /// Returns if the timestamp matches the affinity represented by the slot.
-    fn matches<T: TimeZone>(&self, ts: DateTime<T>) -> bool {
+    /// Returns if `ts` matches the affinity represented by the slot.
+    fn matches_chrono<T: TimeZone>(&self, ts: DateTime<T>) -> bool {
         match self {
-            Slot::Hour(hour_slot) => hour_slot.matches(ts.hour()),
-            Slot::Week(week_slot) => week_slot.matches(ts.weekday().into()),
+            Slot::Hour(hour_slot) => hour_slot.matches_chrono(ts),
+            Slot::Week(week_slot) => week_slot.matches_chrono(ts),
+        }
+    }
+
+    /// Returns the [`TimeDelta`] that `ts` must advance to fit within the slot.
+    fn fwd_delta_chrono<T: TimeZone>(&self, ts: DateTime<T>) -> TimeDelta {
+        match self {
+            Slot::Hour(hour_slot) => hour_slot.fwd_delta_chrono(ts),
+            Slot::Week(week_slot) => week_slot.fwd_delta_chrono(ts),
         }
     }
 }
